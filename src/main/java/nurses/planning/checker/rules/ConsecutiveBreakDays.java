3d3a32954034573cc1c32daae7d496eeeec53caa
Matthias Percelay <matthias.percelay1@gmail.com>
2019-12-03 15:28:46 +0100

    Completed ConsecutiveBreakDays.

--- src/main/java/nurses/planning/checker/rules/ConsecutiveBreakDays.java
+++ src/main/java/nurses/planning/checker/rules/ConsecutiveBreakDays.java
@@ -13,18 +13,25 @@ public class ConsecutiveBreakDays implements IRule {
         int cycle = 0;
         int breaks = 0;
         int maxBreaks = 0;
+        int totalBreaks = 0;
         for (int i = 0; i < agentSchedule.length; i++) {
             cycle++;
             Shift shift = agentSchedule[i];
 
             if (shift.isBreak()) {
                 breaks++;
+                totalBreaks++;
                 if (breaks > maxBreaks) maxBreaks = breaks;
             } else breaks = 0;
 
             if (cycle == 13) {
                 cycle = 0;
-                if (maxBreaks < 2) return false;
+                if (maxBreaks < 2 || totalBreaks < 4) return false;
+                else {
+                    maxBreaks = 0;
+                    totalBreaks = 0;
+                    breaks = 0;
+                }
             }
         }
         return true;
